02d2d85459e4c27983537187882f69ad46dab1f0
Doreen <Doreen.Limbach@gmx.de>
2015-07-22 12:47:56 +0200

    Revert "Revert "Berechnung Variablen ausschreiben""
    This reverts commit ddc375da30a6ffbf314a2d6be8be5f4bf969da28.

--- Baugrundberechnung/Berechnung.cs
+++ Baugrundberechnung/Berechnung.cs
@@ -14,109 +14,109 @@ namespace Baugrundberechnung
         /// <summary>
         /// die Methode berechnung berechnet die Formel für die Längsseite, Sitrnseite und Ecke ohne Be.
         /// </summary>
-        /// <param name="A"></param>
-        /// <param name="U"></param>
-        /// <param name="L"></param>
-        /// <param name="B"></param>
-        /// <param name="H"></param>
-        /// <param name="S"></param>
-        /// <param name="Y"></param>
-        /// <param name="n"></param>
+        /// <param name="raeumlicheAnströmung"></param>
+        /// <param name="einflussUmfeld"></param>
+        /// <param name="laenge"></param>
+        /// <param name="breite"></param>
+        /// <param name="wasserspiegeldifferenz"></param>
+        /// <param name="aquifermaechtigkeit"></param>
+        /// <param name="wichteAuftrieb"></param>
+        /// <param name="globaleSicherheit"></param>
         /// <returns></returns>
-        private static double berechnung(double A, double U, double L, double B, double H, double S, double Y, double n)
+        private static double berechnung(double raeumlicheAnströmung, double einflussUmfeld, double laenge, double breite, double wasserspiegeldifferenz, double aquifermaechtigkeit, double wichteAuftrieb, double globaleSicherheit)
         {
-            double x = 0.32 * A; 
-            double y = (1.244 - 0.32 * A) * Math.Exp((-B / H) / (U * (0.541 + 0.395 * (1 - Math.Exp(1 - S / H))) * (1 + (B / L - 0.3) * (3.156 - 1.564 * U))));
-            double z = Math.Pow((11 / (Y * 0.902 + 1.078) * (n / (1.3 / 0.95))), Math.Sqrt(2));
+            double x = 0.32 * raeumlicheAnströmung;
+            double y = (1.244 - 0.32 * raeumlicheAnströmung) * Math.Exp((-breite / wasserspiegeldifferenz) / (einflussUmfeld * (0.541 + 0.395 * (1 - Math.Exp(1 - aquifermaechtigkeit / wasserspiegeldifferenz))) * (1 + (breite / laenge - 0.3) * (3.156 - 1.564 * einflussUmfeld))));
+            double z = Math.Pow((11 / (wichteAuftrieb * 0.902 + 1.078) * (globaleSicherheit / (1.3 / 0.95))), Math.Sqrt(2));
             return (x + y) * z;
         }
         /// <summary>
         /// die Methode berechnungeben berechnet die Formel für die seite eben.
         /// </summary>
-        /// <param name="A"></param>
-        /// <param name="U"></param>
-        /// <param name="L"></param>
-        /// <param name="B"></param>
-        /// <param name="H"></param>
-        /// <param name="S"></param>
-        /// <param name="Y"></param>
-        /// <param name="n"></param>
+        /// <param name="raeumlicheAnströmung"></param>
+        /// <param name="einflussUmfeld"></param>
+        /// <param name="laenge"></param>
+        /// <param name="breite"></param>
+        /// <param name="wasserspiegeldifferenz"></param>
+        /// <param name="aquifermaechtigkeit"></param>
+        /// <param name="wichteAuftrieb"></param>
+        /// <param name="globaleSicherheit"></param>
         /// <returns></returns>
-        private static double berechnungeben(double A, double U, double L, double B, double H, double S, double Y, double n)
+        private static double berechnungeben(double raeumlicheAnströmung, double einflussUmfeld, double laenge, double breite, double wasserspiegeldifferenz, double aquifermaechtigkeit, double wichteAuftrieb, double globaleSicherheit)
         {
-            double x = 0.32 * A;
-            double y = (1.244 - 0.32 * A) * Math.Exp((-B / H) / (U * (0.541 + 0.395 * (1 - Math.Exp(1 - S / H)))));
-            double z = Math.Pow((11 / (Y * 0.902 + 1.078) * (n / (1.3 / 0.95))), Math.Sqrt(2));
+            double x = 0.32 * raeumlicheAnströmung;
+            double y = (1.244 - 0.32 * raeumlicheAnströmung) * Math.Exp((-breite / wasserspiegeldifferenz) / (einflussUmfeld * (0.541 + 0.395 * (1 - Math.Exp(1 - aquifermaechtigkeit / wasserspiegeldifferenz)))));
+            double z = Math.Pow((11 / (wichteAuftrieb * 0.902 + 1.078) * (globaleSicherheit / (1.3 / 0.95))), Math.Sqrt(2));
             return (x + y) * z;
         }
         /// <summary>
         /// Diese Methode ruft berechnung auf und Multipliziert mit be. Zum Richtigen runden auf die 2. Nachkommastelle wird 0.005 addiert.
         /// </summary>
-        /// <param name="A"></param>
-        /// <param name="U"></param>
+        /// <param name="raeumlicheAnströmung"></param>
+        /// <param name="einflussUmfeld"></param>
         /// <param name="be"></param>
-        /// <param name="L"></param>
-        /// <param name="B"></param>
-        /// <param name="H"></param>
-        /// <param name="S"></param>
-        /// <param name="Y"></param>
-        /// <param name="n"></param>
+        /// <param name="laenge"></param>
+        /// <param name="breite"></param>
+        /// <param name="wasserspiegeldifferenz"></param>
+        /// <param name="aquifermaechtigkeit"></param>
+        /// <param name="wichteAuftrieb"></param>
+        /// <param name="globaleSicherheit"></param>
         /// <returns></returns>
-        public static double berechnungMitBe(double A, double U, double be, double L, double B, double H, double S, double Y, double n)
+        public static double berechnungMitBe(double raeumlicheAnströmung, double einflussUmfeld, double be, double laenge, double breite, double wasserspiegeldifferenz, double aquifermaechtigkeit, double wichteAuftrieb, double globaleSicherheit)
         {
-            return Math.Round((be * berechnung(A, U, L, B, H, S, Y, n) * H) + 0.005, 2);
+            return Math.Round((be * berechnung(raeumlicheAnströmung, einflussUmfeld, laenge, breite, wasserspiegeldifferenz, aquifermaechtigkeit, wichteAuftrieb, globaleSicherheit) * wasserspiegeldifferenz) + 0.005, 2);
 
         }
         /// <summary>
         /// Diese Methode gibt den Wert der berechnung zurück. Zum Richtigen runden auf die 2. Nachkommastelle wird 0.005 addiert
         /// </summary>
-        /// <param name="A"></param>
-        /// <param name="U"></param>
-        /// <param name="L"></param>
-        /// <param name="B"></param>
-        /// <param name="H"></param>
-        /// <param name="S"></param>
-        /// <param name="Y"></param>
-        /// <param name="n"></param>
+        /// <param name="raeumlicheAnströmung"></param>
+        /// <param name="einflussUmfeld"></param>
+        /// <param name="laenge"></param>
+        /// <param name="breite"></param>
+        /// <param name="wasserspiegeldifferenz"></param>
+        /// <param name="aquifermaechtigkeit"></param>
+        /// <param name="wichteAuftrieb"></param>
+        /// <param name="globaleSicherheit"></param>
         /// <returns></returns>
-        public static double berechnungOhneBe(double A, double U, double L, double B, double H, double S, double Y, double n)
+        public static double berechnungOhneBe(double raeumlicheAnströmung, double einflussUmfeld, double laenge, double breite, double wasserspiegeldifferenz, double aquifermaechtigkeit, double wichteAuftrieb, double globaleSicherheit)
         {
-            return Math.Round((berechnung(A, U, L, B, H, S, Y, n) * H) + 0.005, 2);
+            return Math.Round((berechnung(raeumlicheAnströmung, einflussUmfeld, laenge, breite, wasserspiegeldifferenz, aquifermaechtigkeit, wichteAuftrieb, globaleSicherheit) * wasserspiegeldifferenz) + 0.005, 2);
 
         }
         /// <summary>
         /// Diese Methode ruft berechnungeben auf und Multipliziert mit be. Zum Richtigen runden auf die 2. Nachkommastelle wird 0.005 addiert.
         /// </summary>
-        /// <param name="A"></param>
-        /// <param name="U"></param>
+        /// <param name="raeumlicheAnströmung"></param>
+        /// <param name="einflussUmfeld"></param>
         /// <param name="be"></param>
-        /// <param name="L"></param>
-        /// <param name="B"></param>
-        /// <param name="H"></param>
-        /// <param name="S"></param>
-        /// <param name="Y"></param>
-        /// <param name="n"></param>
+        /// <param name="laenge"></param>
+        /// <param name="breite"></param>
+        /// <param name="wasserspiegeldifferenz"></param>
+        /// <param name="aquifermaechtigkeit"></param>
+        /// <param name="wichteAuftrieb"></param>
+        /// <param name="globaleSicherheit"></param>
         /// <returns></returns>
-        public static double berechnungebenMitBe(double A, double U, double be, double L, double B, double H, double S, double Y, double n)
+        public static double berechnungebenMitBe(double raeumlicheAnströmung, double einflussUmfeld, double be, double laenge, double breite, double wasserspiegeldifferenz, double aquifermaechtigkeit, double wichteAuftrieb, double globaleSicherheit)
         {
-            return Math.Round((be * berechnungeben(A, U, L, B, H, S, Y, n) * H) + 0.005, 2);
+            return Math.Round((be * berechnungeben(raeumlicheAnströmung, einflussUmfeld, laenge, breite, wasserspiegeldifferenz, aquifermaechtigkeit, wichteAuftrieb, globaleSicherheit) * wasserspiegeldifferenz) + 0.005, 2);
 
         }
         /// <summary>
         /// Diese Methode gibt den Wert der berechnungeben zurück. Zum Richtigen runden auf die 2. Nachkommastelle wird 0.005 addiert
         /// </summary>
-        /// <param name="A"></param>
-        /// <param name="U"></param>
-        /// <param name="L"></param>
-        /// <param name="B"></param>
-        /// <param name="H"></param>
-        /// <param name="S"></param>
-        /// <param name="Y"></param>
-        /// <param name="n"></param>
+        /// <param name="raeumlicheAnströmung"></param>
+        /// <param name="einflussUmfeld"></param>
+        /// <param name="laenge"></param>
+        /// <param name="breite"></param>
+        /// <param name="wasserspiegeldifferenz"></param>
+        /// <param name="aquifermaechtigkeit"></param>
+        /// <param name="wichteAuftrieb"></param>
+        /// <param name="globaleSicherheit"></param>
         /// <returns></returns>
-        public static double berechnungebenOhneBe(double A, double U, double L, double B, double H, double S, double Y, double n)
+        public static double berechnungebenOhneBe(double raeumlicheAnströmung, double einflussUmfeld, double laenge, double breite, double wasserspiegeldifferenz, double aquifermaechtigkeit, double wichteAuftrieb, double globaleSicherheit)
         {
-            return Math.Round((berechnungeben(A, U, L, B, H, S, Y, n) * H) + 0.005, 2);
+            return Math.Round((berechnungeben(raeumlicheAnströmung, einflussUmfeld, laenge, breite, wasserspiegeldifferenz, aquifermaechtigkeit, wichteAuftrieb, globaleSicherheit) * wasserspiegeldifferenz) + 0.005, 2);
 
         }
     }
